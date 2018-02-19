70663db384b6c58c7c8762d6250f1d0e79008546
RampageFTW <34176175+RampageFTW@users.noreply.github.com>
2018-02-19 05:38:12 -0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -12,17 +12,4 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-        
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-DISABLE_COLLECTSTATIC=1
     execute_from_command_line(sys.argv)
